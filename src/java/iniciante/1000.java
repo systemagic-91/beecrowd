9451cd45acfa6f860250fef14d30407133fd13fc
Rayane Paiva <rayanepaiiva@live.com>
2022-03-09 19:48:12 -0300

    refatoração

---
+++ src/java/iniciante/1000.java
@@ -0,0 +1,14 @@
+package iniciante;
+
+import java.io.IOException;
+ 
+/**
+ * Hello Word
+ */
+
+class Main { 
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello Word");
+    }
+ 
+}
