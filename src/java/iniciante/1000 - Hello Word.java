e9070a0e0fe8bc41badb3fc8a0e34cfb90c6d930
Rayane Paiva <rayanepaiiva@live.com>
2022-03-09 19:37:04 -0300

    beecrowd 1000

---
+++ src/java/iniciante/1000 - Hello Word.java
@@ -0,0 +1,12 @@
+package iniciante;
+
+import java.io.IOException;
+ 
+
+class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello Word");
+    }
+ 
+}
