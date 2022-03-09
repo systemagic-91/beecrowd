8e2a1add5de61865e1e616ef9e189edd2fb866a5
Rayane Paiva <rayanepaiiva@live.com>
2022-03-09 20:52:19 -0300

    refatoraçao

--- src/java/iniciante/Bee1004.java
+++ src/java/iniciante/Bee1004.java
@@ -4,15 +4,16 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
- * Soma Simples
+ * Produto Simples
  */
-public class Bee1003 {
+
+public class Bee1004 {
     public static void main(String[] args) throws IOException{
         Scanner scan = new Scanner(System.in);
 
         int a = scan.nextInt();
-        int b = scan.nextInt();
-        
-        System.out.println("X = " + (a + b));
+        int b = scan.nextInt();     
+
+        System.out.println("PROD = " + (a * b));   
     }
 }
