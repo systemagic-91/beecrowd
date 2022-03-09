9dbfae9ead8b1a5212c10557ce83724e3beca961
Rayane Paiva <rayanepaiiva@live.com>
2022-03-09 20:26:12 -0300

    beecrowd 1003

---
+++ src/java/iniciante/Bee1003.java
@@ -0,0 +1,17 @@
+package iniciante;
+
+import java.util.Scanner;
+
+/**
+ * Soma Simples
+ */
+public class Bee1003 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        
+        System.out.println("X = " + (a + b));
+    }
+}
