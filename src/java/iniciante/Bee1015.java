d693d8cdbcd7d6c02e127a0251cf6466d54f4197
Rayane Paiva <rayanepaiiva@live.com>
2022-06-06 23:09:49 -0300

    beecrowd 1015

---
+++ src/java/iniciante/Bee1015.java
@@ -0,0 +1,24 @@
+package iniciante;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Bee1015 {
+    
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        
+        double x1, y1, x2, y2, distancia;
+
+        x1 = scan.nextDouble();
+        y1 = scan.nextDouble();
+        x2 = scan.nextDouble();
+        y2 = scan.nextDouble();
+
+        distancia = Math.sqrt(Math.pow((x2 - x1), 2)+ Math.pow((y2-y1),2));
+
+        System.out.printf("%.4f%n", distancia);
+    }
+}
