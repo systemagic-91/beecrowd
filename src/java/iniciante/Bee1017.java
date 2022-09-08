d8c54fe138ec9ce1ce485c67a6fea901a08fceb5
Rayane Paiva <rayanepaiiva@live.com>
2022-09-08 18:42:09 -0300

    beecrowd 1017

---
+++ src/java/iniciante/Bee1017.java
@@ -0,0 +1,22 @@
+package iniciante;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+ 
+public class Bee1017 {
+ 
+    public static void main(String[] args) throws IOException {
+         
+        Scanner scan = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        double tempo_gasto_viagem = scan.nextInt();
+        double velocidade_media = scan.nextInt();
+
+        double litros_necessarios = (tempo_gasto_viagem*velocidade_media)/12;        
+        
+        System.out.printf("%.3f%n", litros_necessarios); 
+    }
+ 
+}
