614d744884138960ebb0cb1131990318d0122fac
Rayane Paiva <rayanepaiiva@live.com>
2022-09-08 19:37:31 -0300

    beecrowd 1018

---
+++ src/java/iniciante/Bee1018.java
@@ -0,0 +1,29 @@
+package iniciante;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Bee1018 {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        double valor = scan.nextInt();
+
+        int [] notas = {100, 50, 20, 10, 5, 2, 1};       
+        int [] quant_notas = new int[7];
+        
+        System.out.println((int)valor);
+
+        for (int i = 0; i < quant_notas.length; i++) {            
+            quant_notas[i] = (int)(valor / notas[i]);
+            valor = valor % notas[i];            
+        }        
+
+        for (int i = 0; i < quant_notas.length; i++) {
+            System.out.printf("%d nota(s) de R$ %d,00\n", quant_notas[i], notas[i]);
+        }
+        System.out.println("");
+    }
+}
