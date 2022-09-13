2784fc89767fe43ffc1363ae5d691543e7510cb2
Rayane Paiva <rayanepaiiva@live.com>
2022-09-13 13:27:54 -0300

    beecrowd 1095

---
+++ src/java/iniciante/Bee1095.java
@@ -0,0 +1,17 @@
+package iniciante;
+
+public class Bee1095 {
+    public static void main(String[] args) {
+
+        int i = 1;
+        int j = 60;
+
+        while (j >= 0) {
+            
+            System.out.println("I=" + i + " J=" + j);
+
+            i = i + 3;
+            j = j - 5;
+        }
+    }
+}
