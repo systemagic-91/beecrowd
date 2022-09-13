f62e23a4ee80d270e270415b4f519894b07baa51
Rayane Paiva <rayanepaiiva@live.com>
2022-09-13 13:33:47 -0300

    beecrowd 1096

---
+++ src/java/iniciante/Bee1096.java
@@ -0,0 +1,12 @@
+package iniciante;
+
+public class Bee1096 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 9; i=i+2) {
+            for (int j = 7; j >= 5; j--) {
+                System.out.println("I=" + i + " J=" + j);
+            }
+        }
+    }
+}
