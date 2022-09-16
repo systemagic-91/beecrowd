68c58f74f268ff1759e8cdd2fa5faecefe281d1e
Rayane Paiva <rayanepaiiva@live.com>
2022-09-16 16:39:41 -0300

    beecrowd 1155

---
+++ src/java/iniciante/Bee1155.java
@@ -0,0 +1,13 @@
+package iniciante;
+
+public class Bee1155 {
+    public static void main(String[] args) {
+        
+        double s = 0.0;
+
+        for (int i = 1; i <= 100; i++) 
+            s += 1.0/i;    
+        
+        System.out.printf("%.2f\n", s);
+    }
+}
